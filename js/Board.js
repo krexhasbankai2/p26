295ddf174c7b555cc77000e88c8185785c59a73a
krexhasbankai2 <87955280+krexhasbankai2@users.noreply.github.com>
2022-02-17 20:56:30 -0500

    Add files via upload

---
+++ js/Board.js
@@ -0,0 +1,29 @@
+class Board{
+    constructor(x,y,width,height){
+        const options = {
+            isStatic: true
+          };
+      
+          this.body = Matter.Bodies.rectangle(x, y, width, height, options);
+          this.width = width;
+          this.height = height;
+            this.image = loadImage("./assets/board.png");
+      
+          World.add(world, this.body);
+      
+
+    }
+    display() {
+        var pos = this.body.position;
+        var angle = this.body.angle;
+    
+       
+        push();
+        translate(pos.x, pos.y);
+        rotate(angle);
+        imageMode(CENTER);
+        image(this.image, 0, 0, this.width, this.height);
+        pop();
+      }
+    
+}
